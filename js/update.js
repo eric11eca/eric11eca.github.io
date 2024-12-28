98625db3135743ccb2ca3494ef443769b0eb4b25
Eric <zeming.chen@epfl.ch>
2024-12-28 19:51:27 +0100

    Update recent publications

--- js/update.js
+++ js/update.js
@@ -74,211 +74,6 @@ document.addEventListener('DOMContentLoaded', function() {
     .catch(error => console.error('Error loading JSON:', error));
 });
 
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   fetch('../data/papers.json')
-//     .then(response => response.json())
-//     .then(data => {
-//       let paperItems = document.getElementById('paper-items');
-
-//       // Create sections for each category
-//       const preprintsSection = document.createElement('div');
-//       preprintsSection.id = 'preprints-section';
-//       preprintsSection.innerHTML = '<h2>Preprints</h2>';
-//       paperItems.appendChild(preprintsSection);
-
-//       const conferencesSection = document.createElement('div');
-//       conferencesSection.id = 'conferences-section';
-//       conferencesSection.innerHTML = '<h2>Conferences & Journals</h2>';
-//       paperItems.appendChild(conferencesSection);
-
-//       const workshopsSection = document.createElement('div');
-//       workshopsSection.id = 'workshops-section';
-//       workshopsSection.innerHTML = '<h2>Workshops</h2>';
-//       paperItems.appendChild(workshopsSection);
-
-//       data.forEach(item => {
-//         const whiteWrapper = document.createElement('div');
-//         whiteWrapper.className = 'white-wrapper';
-
-//         const rowDiv = document.createElement('div');
-//         rowDiv.className = 'w-row';
-
-//         // Image Column
-//         const imageCol = document.createElement('div');
-//         imageCol.className = 'column-17 w-col w-col-4';
-//         if (item.imageSrc.includes('meditron')) {
-//           imageCol.style = 'margin-top: 5%;';
-//         }
-//         const img = document.createElement('img');
-//         img.src = item.imageSrc;
-//         img.srcset = item.imageSrcset;
-//         img.alt = ''; // Add appropriate alt text for each image
-//         img.loading = 'lazy';
-//         img.width = '2839';
-//         img.height = '240';
-//         img.className = 'image-9';
-//         imageCol.appendChild(img);
-
-//         // Content Column
-//         const contentCol = document.createElement('div');
-//         contentCol.className = 'w-col w-col-8';
-
-//         // Title Section
-//         const titleSection = document.createElement('div');
-//         titleSection.className = 'wf-section';
-//         const title = document.createElement('h2');
-//         title.className = 'heading-15';
-//         title.textContent = item.title;
-//         titleSection.appendChild(title);
-
-//         // Links Section
-//         const linksSection = document.createElement('div');
-//         linksSection.className = 'wf-section';
-//         const mobileMask = document.createElement('div');
-//         mobileMask.className = 'post-info mobile-mask';
-//         mobileMask.textContent = '|';
-
-//         // PDF Link
-//         if (item.pdfLink) {
-//           const pdfAnchor = document.createElement('a');
-//           pdfAnchor.href = item.pdfLink;
-//           pdfAnchor.target = '_blank';
-//           pdfAnchor.className = 'label_link';
-//           const pdfSpan = document.createElement('span');
-//           pdfSpan.className = 'post-info label label-primary';
-//           pdfSpan.textContent = 'pdf';
-//           pdfAnchor.appendChild(pdfSpan);
-//           linksSection.appendChild(pdfAnchor);
-//         }
-
-//         // Code Link
-//         if (item.codeLink) {
-//           const codeAnchor = document.createElement('a');
-//           codeAnchor.href = item.codeLink;
-//           codeAnchor.target = '_blank';
-//           codeAnchor.className = 'label_link';
-//           const codeSpan = document.createElement('span');
-//           codeSpan.className = 'post-info label label-code';
-//           codeSpan.textContent = 'code';
-//           codeAnchor.appendChild(codeSpan);
-//           linksSection.appendChild(mobileMask);
-//           linksSection.appendChild(codeAnchor);
-//         }
-
-//         // Video Link
-//         if (item.videoLink) {
-//           const videoAnchor = document.createElement('a');
-//           videoAnchor.href = item.videoLink;
-//           videoAnchor.target = '_blank';
-//           videoAnchor.className = 'label_link';
-//           const videoSpan = document.createElement('span');
-//           videoSpan.className = 'post-info label label-video';
-//           videoSpan.textContent = 'video';
-//           videoAnchor.appendChild(videoSpan);
-//           linksSection.appendChild(mobileMask.cloneNode(true));
-//           linksSection.appendChild(videoAnchor);
-//         }
-
-//         // Conference Link
-//         if (item.conferenceLink) {
-//           const conferenceAnchor = document.createElement('a');
-//           conferenceAnchor.href = item.conferenceLink;
-//           conferenceAnchor.target = '_blank';
-//           conferenceAnchor.className = 'label_link';
-//           const conferenceSpan = document.createElement('span');
-//           conferenceSpan.className = 'post-info label label-conference';
-//           conferenceSpan.textContent = item.conferenceLabel;
-//           conferenceAnchor.appendChild(conferenceSpan);
-//           linksSection.appendChild(mobileMask.cloneNode(true));
-//           linksSection.appendChild(conferenceAnchor);
-//         }
-
-//         // Workshop Link
-//         if (item.workshopLink) {
-//           const workshopAnchor = document.createElement('a');
-//           workshopAnchor.href = item.workshopLink;
-//           workshopAnchor.target = '_blank';
-//           workshopAnchor.className = 'label_link';
-//           const workshopSpan = document.createElement('span');
-//           workshopSpan.className = 'post-info label label-workshop';
-//           workshopSpan.textContent = item.workshopLabel;
-//           workshopAnchor.appendChild(workshopSpan);
-//           linksSection.appendChild(mobileMask.cloneNode(true));
-//           linksSection.appendChild(workshopAnchor);
-//         }
-
-//         // Award Link
-//         if (item.awardLink) {
-//           const awardAnchor = document.createElement('a');
-//           awardAnchor.href = item.awardLink;
-//           awardAnchor.target = '_blank';
-//           awardAnchor.className = 'label_link';
-//           const awardSpan = document.createElement('span');
-//           awardSpan.className = 'post-info label label-best';
-//           awardSpan.textContent = item.awardLabel;
-//           awardAnchor.appendChild(awardSpan);
-//           linksSection.appendChild(mobileMask.cloneNode(true));
-//           linksSection.appendChild(awardAnchor);
-//         }
-
-//         // Authors and Publication Info
-//         const infoSection = document.createElement('div');
-//         infoSection.className = 'wf-section';
-//         const authorsInfo = document.createElement('div');
-//         authorsInfo.className = 'post-info-noncaps';
-//         authors = `✍️ <i>${item.authors}</i><br>`;
-//         venue = `📚 <font size="+0">${item.publicationInfo}</font><br>`;
-//         publication = `<br>${authors}${venue}<br>`.replace('Zeming Chen', '<b>Zeming Chen</b>');
-//         authorsInfo.innerHTML = publication;
-//         infoSection.appendChild(authorsInfo);
-
-//         // Summary
-//         const summary = document.createElement('div');
-//         summary.innerHTML = `<b>TL;DR: </b> ${item.summary}`;
-
-//         // Append all sections to content column
-//         contentCol.appendChild(titleSection);
-//         contentCol.appendChild(linksSection);
-//         contentCol.appendChild(infoSection);
-//         // Check and add Presentation Type if available
-//         if (item.presentationType) {
-//           const presentationTypeSection = document.createElement('div');
-//           presentationTypeSection.innerHTML = `<font color="firebrick"><h4><b>${item.presentationType}</b></h4></font>`;
-//           contentCol.appendChild(presentationTypeSection);
-//         }
-//         if (item.mediaInfo && item.mediaLink && Array.isArray(item.mediaInfo) && Array.isArray(item.mediaLink) && item.mediaInfo.length === item.mediaLink.length) {
-//           item.mediaInfo.forEach((info, index) => {
-//             const presentationTypeSection = document.createElement('div');
-//             presentationTypeSection.innerHTML = `<font color="firebrick"><h4><b><a target="_blank" href="${item.mediaLink[index]}">${info}</a></b></h4></font>`;
-//             contentCol.appendChild(presentationTypeSection);
-//           });
-//         } else if (item.mediaInfo && item.mediaLink) {
-//           const presentationTypeSection = document.createElement('div');
-//           presentationTypeSection.innerHTML = `<font color="firebrick"><h4><b><a target="_blank" href=${item.mediaLink}>${item.mediaInfo}</a></b></h4></font>`;
-//           contentCol.appendChild(presentationTypeSection);
-//         }
-//         contentCol.appendChild(summary);
-
-//         // Append columns to row
-//         rowDiv.appendChild(imageCol);
-//         rowDiv.appendChild(contentCol);
-
-//         whiteWrapper.appendChild(rowDiv);
-
-//         // Append to appropriate section
-//         if (item.conferenceLink) {
-//           conferencesSection.appendChild(whiteWrapper);
-//         } else if (item.workshopLink) {
-//           workshopsSection.appendChild(whiteWrapper);
-//         } else {
-//           preprintsSection.appendChild(whiteWrapper);
-//         }
-//       });
-//     })
-//     .catch(error => console.error('Error loading JSON:', error));
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   class PaperManager {
     constructor(dataUrl, containerId) {
@@ -288,6 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
         all: null,
         preprints: null,
         conferences: null,
+        journals: null,
         workshops: null,
       };
       this.initialize();
@@ -318,6 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       this.createButton(buttonBar, 'All', () => this.showSection('all'));
       this.createButton(buttonBar, 'Preprints', () => this.showSection('preprints-section'));
+      this.createButton(buttonBar, 'Journals', () => this.showSection('journals-section'));
       this.createButton(buttonBar, 'Conferences', () => this.showSection('conferences-section'));
       this.createButton(buttonBar, 'Workshops', () => this.showSection('workshops-section'));
     }
@@ -333,10 +130,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     createSections() {
       this.sections.preprints = this.createSection('preprints-section', 'Preprints');
+      this.sections.journals = this.createSection('journals-section', 'Journals');
       this.sections.conferences = this.createSection('conferences-section', 'Conferences');
       this.sections.workshops = this.createSection('workshops-section', 'Workshops');
     }
 
+    // Create a section with a title
     createSection(id, title) {
       const section = this.createElement('div', { id });
       section.innerHTML = `<h2>${title}</h2>`;
@@ -344,12 +143,15 @@ document.addEventListener('DOMContentLoaded', () => {
       return section;
     }
 
+    // Populate sections with paper items
     populateSections(data) {
       data.forEach(item => {
         const paperElement = this.createPaperElement(item);
 
         if (item.conferenceLink) {
           this.sections.conferences.appendChild(paperElement);
+        } else if (item.journalLink) {
+          this.sections.journals.appendChild(paperElement);
         } else if (item.workshopLink) {
           this.sections.workshops.appendChild(paperElement);
         } else {
@@ -361,6 +163,8 @@ document.addEventListener('DOMContentLoaded', () => {
     createPaperElement(item) {
       const whiteWrapper = this.createElement('div', { className: 'white-wrapper' });
       const rowDiv = this.createElement('div', { className: 'w-row' });
+      rowDiv.style.display = 'flex';
+      rowDiv.style.alignItems = 'center';
 
       const imageCol = this.createImageColumn(item);
       const contentCol = this.createContentColumn(item);
@@ -374,9 +178,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     createImageColumn(item) {
       const imageCol = this.createElement('div', { className: 'column-17 w-col w-col-4' });
-      if (item.imageSrc.includes('meditron')) {
-        imageCol.style.marginTop = '5%';
-      }
       const img = this.createElement('img', {
         src: item.imageSrc,
         srcset: item.imageSrcset,
@@ -392,18 +193,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     createContentColumn(item) {
       const contentCol = this.createElement('div', { className: 'w-col w-col-8' });
-
       const titleSection = this.createElement('div', { className: 'wf-section' }, `<h2 class="heading-15">${item.title}</h2>`);
       const linksSection = this.createLinksSection(item);
       const infoSection = this.createInfoSection(item);
-      const summary = this.createElement('div', {}, `<b>TL;DR: </b> ${item.summary}`);
+      const summary = this.createElement(
+        'details',
+        {}, // any attributes, e.g. { open: true }
+        `
+          <summary><strong>TL;DR</strong></summary>
+          <p>${item.summary}</p>
+        `
+      );
 
       contentCol.appendChild(titleSection);
       contentCol.appendChild(linksSection);
       contentCol.appendChild(infoSection);
-      this.addPresentationType(contentCol, item);
       contentCol.appendChild(summary);
+      contentCol.appendChild(this.createElement('hr'));
+      this.addPresentationType(contentCol, item);
 
+      // Check if 'hr' is the last child and remove it
+      if (contentCol.lastChild.tagName === 'HR') {
+        contentCol.removeChild(contentCol.lastChild);
+      }
       return contentCol;
     }
 
@@ -411,7 +223,10 @@ document.addEventListener('DOMContentLoaded', () => {
       const linksSection = this.createElement('div', { className: 'wf-section' });
       const mobileMask = this.createElement('div', { className: 'post-info mobile-mask' }, '|');
 
-      if (item.pdfLink) this.appendLink(linksSection, item.pdfLink, 'pdf', 'label label-primary');
+      if (item.pdfLink) {
+        this.appendLink(linksSection, item.pdfLink, 'pdf', 'label label-primary');
+        linksSection.appendChild(mobileMask);
+      }
       if (item.codeLink) {
         this.appendLink(linksSection, item.codeLink, 'code', 'label label-code');
         linksSection.appendChild(mobileMask);
@@ -424,6 +239,10 @@ document.addEventListener('DOMContentLoaded', () => {
         this.appendLink(linksSection, item.conferenceLink, item.conferenceLabel, 'label label-conference');
         linksSection.appendChild(mobileMask.cloneNode(true));
       }
+      if (item.journalLink) {
+        this.appendLink(linksSection, item.journalLink, item.journalLabel, 'label label-journal');
+        linksSection.appendChild(mobileMask.cloneNode(true));
+      }
       if (item.workshopLink) {
         this.appendLink(linksSection, item.workshopLink, item.workshopLabel, 'label label-workshop');
         linksSection.appendChild(mobileMask.cloneNode(true));
@@ -433,6 +252,7 @@ document.addEventListener('DOMContentLoaded', () => {
         linksSection.appendChild(mobileMask.cloneNode(true));
       }
 
+      linksSection.removeChild(linksSection.lastChild);
       return linksSection;
     }
 
@@ -444,11 +264,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     createInfoSection(item) {
-      const infoSection = this.createElement('div', { className: 'wf-section' });
+      const infoSection = this.createElement('div', { className: 'wf-section', style: 'padding-right: 20px;' });
       const authorsInfo = this.createElement('div', { className: 'post-info-noncaps' });
 
-      const authors = `✍️ <i>${item.authors}</i><br>`;
-      const venue = `📚 <font size="+0">${item.publicationInfo}</font><br>`;
+      const authors = `✍️ <i>${item.authors}</i><br><br>`;
+      const venue = `📚 <font size="+0">${item.publicationInfo}</font>`;
       const publication = `<br>${authors}${venue}<br>`.replace('Zeming Chen', '<b>Zeming Chen</b>');
 
       authorsInfo.innerHTML = publication;
